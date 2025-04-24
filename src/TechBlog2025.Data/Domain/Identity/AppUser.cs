1da9cf936766c90be806a09ae88f2c3eed4fa908
CUN-PC\ADMIN <quocphungccq1911h@gmail.com>
2025-04-24 21:22:28 +0700

    up

---
+++ src/TechBlog2025.Data/Domain/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechBlog2025.Data.Domain.Identity
+{
+    [Table("AppUsers")]
+    public class AppUser : IdentityUser<Guid>
+    {
+    }
+}
